abe33acc535f3796d702c86bb1b2d28986e32147
pclifton <pclifton739@gmail.com>
2022-03-23 14:13:21 -0500

    Updated duck class

---
+++ GoKart/src/main/java/goKart/Kart.java
@@ -0,0 +1,56 @@
+package goKart;
+
+public class Kart {
+    //Fields
+    private String name;
+    private boolean needGas;
+    private String color;
+    private int laps;
+
+    //Constructor
+    public Kart() {
+    }
+
+    //Parameterized Constructor
+    public Kart(String name, boolean needGas, String color, int laps) {
+        this.name = name;
+        this.needGas = needGas;
+        this.color = color;
+        this.laps = laps;
+    }
+
+    //Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isNeedGas() {
+        return needGas;
+    }
+
+    public void setNeedGas(boolean needGas) {
+        this.needGas = needGas;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getLaps() {
+        return laps;
+    }
+
+    public void setLaps(int laps) {
+        this.laps = laps;
+    }
+
+}
--- Zoo/src/main/java/zoo/Duck.java
+++ Zoo/src/main/java/zoo/Duck.java
@@ -1,4 +1,31 @@
 package zoo;
 
-public class Duck {
+public class Duck extends Animal{
+    //Fields
+    private static final String BEAKCOLOR = "Yellow";
+
+    //Constructors
+    public Duck(int age, String gender, String name) {
+        super(age, gender, name);
+    }
+
+    //Methods
+    public String quack(){
+        return "I am quacking!";
+    }
+    public String fly(){
+        return "I am flying";
+    }
+    //Override Methods
+    @Override
+    public String swim() {
+        return "I am swimming as a Duck!";
+    }
+
+    //To String
+    public String toString(){
+        return "The Duck's name is "+ getName()+
+                " Age: "+getAge()+
+                " Beak color is: "+BEAKCOLOR;
+    }
 }
